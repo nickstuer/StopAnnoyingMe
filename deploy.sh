76227be395085d1bbf1d58a53df5ad6e4c9f1661
Nick Stuer <nickstuer@gmail.com>
2025-12-10 21:18:42 -0500

    refactor: Change file structure, use bigwigspackage release process

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sourceFile=$(find src/* -name "*.toc")
+sourceFile=$(find . -maxdepth 1 -name "*.toc")
 addonName="Unknown"
 addonNameRegexPattern="## Title: ([A-Za-z]+)"
 
@@ -14,6 +14,7 @@ done < $sourceFile
 
 # Retail WoW
 echo "Deploying $addonName"
+
 ADDON_PATH="/Applications/World of Warcraft/_retail_/Interface/AddOns/${addonName}"
 ADDON_PATH_WINDOWS="C:\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns\\${addonName}"
 
@@ -25,45 +26,9 @@ echo "Using path: $ADDON_PATH"
 rm -vrf "$ADDON_PATH" | echo "$(wc -l) files deleted"
 mkdir -p "$ADDON_PATH"
 
-cp -R -a "src/." "${ADDON_PATH}";
-cp -R "README.md" "${ADDON_PATH}/README.md";
+find . -maxdepth 1 \( -type f -o -type d \) -not -path "./.github*" -not -path "./.git*" -not -path "./dist*" -not -path "." -exec cp -R {} "${ADDON_PATH}/" \;
 
 echo "Successfully deployed to $ADDON_PATH"
 
-# Classic Cata WoW
-echo "Deploying $addonName"
-ADDON_PATH="/Applications/World of Warcraft/_classic_/Interface/AddOns/${addonName}"
-ADDON_PATH_WINDOWS="C:\Program Files (x86)\World of Warcraft\_classic_\Interface\AddOns\\${addonName}"
-
-if [ "$OSTYPE" == "msys" ]; then
-    ADDON_PATH="$ADDON_PATH_WINDOWS"
-fi
-echo "Using path: $ADDON_PATH"
-
-rm -vrf "$ADDON_PATH" | echo "$(wc -l) files deleted"
-mkdir -p "$ADDON_PATH"
-
-cp -R -a "src/." "${ADDON_PATH}";
-cp -R "README.md" "${ADDON_PATH}/README.md";
-
-echo "Successfully deployed to $ADDON_PATH"
-
-# Classic  WoW
-echo "Deploying $addonName"
-ADDON_PATH="/Applications/World of Warcraft/_classic_era_/Interface/AddOns/${addonName}"
-ADDON_PATH_WINDOWS="C:\Program Files (x86)\World of Warcraft\_classic_era_\Interface\AddOns\\${addonName}"
-
-if [ "$OSTYPE" == "msys" ]; then
-    ADDON_PATH="$ADDON_PATH_WINDOWS"
-fi
-echo "Using path: $ADDON_PATH"
-
-rm -vrf "$ADDON_PATH" | echo "$(wc -l) files deleted"
-mkdir -p "$ADDON_PATH"
-
-cp -R -a "src/." "${ADDON_PATH}";
-cp -R "README.md" "${ADDON_PATH}/README.md";
-
-echo "Successfully deployed to $ADDON_PATH"
-exit
-
+sed -i.bak 's/@project-version@/v0.0.0/g' "$ADDON_PATH"/*.toc
+echo "Updated version in .toc file"
